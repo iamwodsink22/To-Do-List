6bed9469e0eb1011c25fcf02509a4590fb6cdc8d
Araksha <arakshapuri22@gmail.com>
2022-06-07 17:07:54 +0545

    Committed again

--- list.js
+++ list.js
@@ -1,5 +1,4 @@
-app = document.getElementById('submit');
-app.addEventListener("click", ()=>{
+function add(){
     console.log("Updating list...");
     nem=document.getElementById('text').value;
     description=document.getElementById('pop').value;
@@ -15,4 +14,24 @@ else{
     localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
 }
 
-});
+
+ table=document.getElementById('table');
+str="";
+itemJsonArray.forEach((element, index) => {
+    str+= `
+    <tr>
+                <td>${index+1}</td>
+                <td>${element[0]}</td>
+                <td>${element[1]}</td>
+                <td><button>Done</button></td>
+            </tr>`;
+    
+}); 
+table.innerHTML = str;
+}
+
+app = document.getElementById('submit');
+
+app.addEventListener("click", add);
+
+   
